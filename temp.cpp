97545acbd6f3de141aec3f9a0b1b603e85d295b3
dvpatel852 <dvpatel852@gmail.com>
2022-04-23 15:00:49 +0530

    C++ Programs

---
+++ temp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[1][1];
+    cout<<"Enter a11 = ";
+    cin>>a[0][0];
+    cout<<"Your matrix is\n|";
+    cout<<a[0][0]<<"|";
+    return 0;
+}
